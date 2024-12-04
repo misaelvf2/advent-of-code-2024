4d011fbb188a4e86bce431555170f72deb34e9f3
Misael Valentin <misael.vf2@gmail.com>
2024-12-03 19:08:32 -0500

    Finish day 2

---
+++ day2.py
@@ -0,0 +1,60 @@
+import itertools
+
+from cli import create_day_cli
+
+
+def parse_input(path):
+    with open(path) as f:
+        return [[int(x) for x in line.split()] for line in f]
+
+
+def part1(reports):
+    return sum(
+        all(
+            gradually_increasing(previous, current, lower_limit=1, upper_limit=3)
+            for previous, current in itertools.pairwise(report)
+        )
+        or all(
+            gradually_decreasing(previous, current, lower_limit=1, upper_limit=3)
+            for previous, current in itertools.pairwise(report)
+        )
+        for report in reports
+    )
+
+
+def part2(reports):
+    safe = 0
+    for report in reports:
+        for i in range(len(report)):
+            if all(
+                gradually_increasing(previous, current, lower_limit=1, upper_limit=3)
+                for previous, current in itertools.pairwise(
+                    report[:i] + report[i + 1 :]
+                )
+            ) or all(
+                gradually_decreasing(previous, current, lower_limit=1, upper_limit=3)
+                for previous, current in itertools.pairwise(
+                    report[:i] + report[i + 1 :]
+                )
+            ):
+                safe += 1
+                break
+    return safe
+
+
+def gradually_increasing(x, y, lower_limit, upper_limit):
+    return lower_limit <= x - y <= upper_limit
+
+
+def gradually_decreasing(x, y, lower_limit, upper_limit):
+    return lower_limit <= y - x <= upper_limit
+
+
+def adjacent_diffs(report):
+    return [report[i] - report[i - 1] for i in range(len(report)) if i > 0]
+
+
+app = create_day_cli(day_number=2, input_parser=parse_input, part1=part1, part2=part2)
+
+if __name__ == "__main__":
+    app()
