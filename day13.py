6bcca208622469aff2f32946271a4b23f00958c2
Misael Valentin <misael.vf2@gmail.com>
2024-12-15 21:11:32 -0500

    Solve day 13 (it's just math)

--- day13.py
+++ day13.py
@@ -1,4 +1,3 @@
-import math
 import re
 from collections import namedtuple
 
@@ -34,33 +33,56 @@ def parse_input(path):
 def part1(machines: list[Machine]):
     tokens = 0
     for machine in machines:
-        # Try the maximum possible number of B presses possible
-        b_presses = min(
-            machine.Prize_X // machine.B_X, machine.Prize_Y // machine.B_Y, 100
-        )
-        while not valid_solution(b_presses, machine) and b_presses >= 0:
-            b_presses -= 1
-        if b_presses >= 0:
-            a_presses = (machine.Prize_X - b_presses * machine.B_X) // machine.A_X
-            cost = (b_presses * 1) + (a_presses * 3)
+        a_presses = (
+            (machine.B_X * machine.Prize_Y) - (machine.Prize_X * machine.B_Y)
+        ) / ((machine.A_Y * machine.B_X) - machine.A_X * machine.B_Y)
+        b_presses = (machine.Prize_X - (a_presses * machine.A_X)) / machine.B_X
+        a_presses = int(a_presses)
+        b_presses = int(b_presses)
+        if valid_solution(a_presses, b_presses, machine):
+            cost = 3 * a_presses + b_presses
             tokens += cost
     return tokens
 
 
-def valid_solution(b_presses, machine: Machine):
-    remaining_x = machine.Prize_X - b_presses * machine.B_X
-    remaining_y = machine.Prize_Y - b_presses * machine.B_Y
-    a_presses_x = remaining_x // machine.A_X
-    a_presses_y = remaining_y // machine.A_Y
-    return (
-        a_presses_x == a_presses_y
-        and remaining_x % a_presses_x == 0
-        and remaining_y % a_presses_y == 0
-    )
+def part2(machines: list[Machine]):
+    tokens = 0
+    for machine in machines:
+        a_presses = (
+            (machine.B_X * (machine.Prize_Y + 10000000000000))
+            - ((machine.Prize_X + 10000000000000) * machine.B_Y)
+        ) / ((machine.A_Y * machine.B_X) - machine.A_X * machine.B_Y)
+        b_presses = (
+            machine.Prize_X + 10000000000000 - (a_presses * machine.A_X)
+        ) / machine.B_X
+        a_presses = int(a_presses)
+        b_presses = int(b_presses)
+        if valid_solution(a_presses, b_presses, machine, part_b=True):
+            cost = 3 * a_presses + b_presses
+            tokens += cost
+    return tokens
 
 
-def part2(machines):
-    pass
+def valid_solution(a_presses, b_presses, machine: Machine, part_b: bool = False):
+    if not part_b:
+        if not (0 <= a_presses <= 100 and 0 <= b_presses <= 100):
+            return False
+        satisfies_X = (
+            a_presses * machine.A_X + b_presses * machine.B_X == machine.Prize_X
+        )
+        satisfies_Y = (
+            a_presses * machine.A_Y + b_presses * machine.B_Y == machine.Prize_Y
+        )
+    else:
+        satisfies_X = (
+            a_presses * machine.A_X + b_presses * machine.B_X
+            == machine.Prize_X + 10000000000000
+        )
+        satisfies_Y = (
+            a_presses * machine.A_Y + b_presses * machine.B_Y
+            == machine.Prize_Y + 10000000000000
+        )
+    return satisfies_X and satisfies_Y
 
 
 if __name__ == "__main__":
